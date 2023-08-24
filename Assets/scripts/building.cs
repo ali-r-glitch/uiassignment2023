bb12e0dbb671b4320c4b5030aaeacbe6e063f859
aliraffee <aliraffee2@gmail.com>
2023-08-24 15:20:14 +0200

    added shit

--- Assets/scripts/building.cs
+++ Assets/scripts/building.cs
@@ -7,32 +7,56 @@ public class building : MonoBehaviour
     // Start is called before the first frame update
 
     public bool isonfire;
-    public float maybefire = 0;
+
+    public float maxhealth,current,maybefire = 0;
     public int randomnum,randomnum2,probabl=20;
-    GameObject canvas,player;
+    public GameObject canvas,player,wall1, wall2, wall3, wall4;
 
     // Update is called once per frame\
 
-
+    private void Start()
+    {
+        canvas = (transform.FindChild("Canvas")).gameObject;
+        player = GameObject.Find("player");
+    }
 
     void ignitebuilding()
     {
         // Find the child with the specified tag
 
-         canvas = (transform.FindChild("Canvas")).gameObject;
-        player = GameObject.Find("player");
+        
         canvas.SetActive(true);
-    
+        current = maxhealth;
+        burningbuilding();
+
+    }
+
+
+    public void burningbuilding()
+    {
+      
+        wall1.transform.localScale += new Vector3(0, 0.05f,0);
+        wall2.transform.localScale += new Vector3(0, 0.05f, 0);
+        wall3.transform.localScale += new Vector3(0, 0.05f, 0);
+        wall4.transform.localScale += new Vector3(0, 0.05f, 0);
+       
+        
+        if (wall1.transform.localScale.y>1f)
+        {
+            this.gameObject.SetActive(false);
+        }
     }
     private void FixedUpdate()
     {
-        Debug.Log(maybefire);
+        //Debug.Log(maybefire);
         maybefire = maybefire + 1 * Time.deltaTime;
-        if(maybefire>1)
+        
+        if (maybefire>1)
         {
+            maybefire -= 1;
             if (!isonfire)
             {
-                maybefire -= 1;
+               
                 randomnum = Random.Range(1, probabl);
                 randomnum2 = Random.Range(1, probabl);
 
@@ -43,10 +67,13 @@ public class building : MonoBehaviour
                 }
             }else
             {
-                canvas.transform.LookAt(player.transform);
+                burningbuilding();
+              
+
             }
         }
-        
+        canvas.transform.LookAt(player.transform);
+
 
 
     }
