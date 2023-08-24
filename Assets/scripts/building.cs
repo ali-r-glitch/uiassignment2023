831da5af02e5b44866c409a2e5882bc009a7cfc0
aliraffee <aliraffee2@gmail.com>
2023-08-24 12:31:19 +0200

    added smoke

--- Assets/scripts/building.cs
+++ Assets/scripts/building.cs
@@ -9,23 +9,47 @@ public class building : MonoBehaviour
     public bool isonfire;
     public float maybefire = 0;
     public int randomnum,randomnum2,probabl=20;
+    GameObject canvas,player;
 
-    // Update is called once per frame
+    // Update is called once per frame\
+
+
+
+    void ignitebuilding()
+    {
+        // Find the child with the specified tag
+
+         canvas = (transform.FindChild("Canvas")).gameObject;
+        player = GameObject.Find("player");
+        canvas.SetActive(true);
+    
+    }
     private void FixedUpdate()
     {
         Debug.Log(maybefire);
         maybefire = maybefire + 1 * Time.deltaTime;
         if(maybefire>1)
         {
-            maybefire -= 1;
-            randomnum = Random.Range(1, probabl);
-            randomnum2 = Random.Range(1, probabl);
-            if (randomnum2 == randomnum)
+            if (!isonfire)
             {
-                isonfire = true;
+                maybefire -= 1;
+                randomnum = Random.Range(1, probabl);
+                randomnum2 = Random.Range(1, probabl);
+
+                if (randomnum2 == randomnum)
+                {
+                    isonfire = true;
+                    ignitebuilding();
+                }
+            }else
+            {
+                canvas.transform.LookAt(player.transform);
             }
         }
+        
 
 
     }
+
+
 }
